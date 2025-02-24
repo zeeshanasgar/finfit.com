ce3a745b35792f246c852f59783024e43a2753df
Zeeshan Asgar <zeeshanasgar07@gmail.com>
2025-02-25 00:31:19 +0530

    Added project files

---
+++ FinanceWithFarid/Controllers/ContactusController.cs
@@ -0,0 +1,76 @@
+﻿using Logger.Data;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Mail;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FinanceWithFarid.Controllers
+{
+    public class ContactusController : Controller
+    {
+        private FinanceWithFaridDataEntities db;/* = new FinanceWithFaridDataEntities();*/
+
+        [HttpPost]
+        public ActionResult AddContactus(contactus contactuss)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    using (var db = new FinanceWithFaridDataEntities())
+                    {
+                        if (contactuss != null)
+                        {
+                            //email
+
+                            //MailMessage mm = new MailMessage("zeeshanasgar07@gmail.com", contactuss.Email);
+                            MailMessage mm = new MailMessage("zeeshanasgar07@gmail.com", "zeeshanasgar07@gmail.com");
+                            mm.Subject = "This is your Data";
+                            string body = $"Name: {contactuss.Name} \n";
+                            body += $"Email: {contactuss.Email} \n";
+                            body += $"Subject: {contactuss.Subject} \n";
+                            body += $"Message: {contactuss.Message}  \n";
+
+                            // for sending image
+                            //string fileName = Path.GetFileName(contactuss.file.FileName);
+                            //mm.Attachments.Add(new Attachment(contactuss.file.InputStream, fileName));
+
+
+                            mm.Body = body;
+                            mm.IsBodyHtml = false;
+
+                            SmtpClient smtp = new SmtpClient();
+                            smtp.Host = "smtp.gmail.com";
+                            smtp.Port = 587;
+                            smtp.EnableSsl = true;
+
+                            NetworkCredential nc = new NetworkCredential("zeeshanasgar07@gmail.com", "desydnsxnecdafeu");
+                            smtp.UseDefaultCredentials = true;
+                            smtp.Credentials = nc;
+                            smtp.Send(mm);
+
+
+                            //email
+
+                            db.contactus.Add(contactuss);
+                            db.SaveChanges();
+                            return Json(new { success = true }); // Return a success response
+                        }
+                    }
+                }
+                // Handle any validation errors or null data
+                return Json(new { success = false, error = "Invalid data" });
+            }
+            catch (Exception ex)
+            {
+                // Log the exception
+                return Json(new { success = false, error = "An error occurred" });
+            }
+        }
+
+    }
+}
---
+++ FinanceWithFarid/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("FinanceWithFarid")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FinanceWithFarid")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("33ea70e2-3e41-473f-8a34-5b47c7807a67")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
